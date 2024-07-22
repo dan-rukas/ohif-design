a3f0683d2389af82fcf50d7c59559007e0cfd335
Dan Rukas <dan.rukas@gmail.com>
2024-07-22 13:30:16 -0400

    Test layouts

---
+++ src/app/viewer/page.tsx
@@ -0,0 +1,14 @@
+// src/app/customPage/page.tsx
+
+import React from 'react';
+
+const CustomPage: React.FC = () => {
+  return (
+    <div>
+      <h2>Custom Page Content</h2>
+      <p>This page does not use the standard layout.</p>
+    </div>
+  );
+};
+
+export default CustomPage;
